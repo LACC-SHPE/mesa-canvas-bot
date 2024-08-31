2966e6dbccca420ef19c1580e433012a65b0b050
dichill <dichilltomarong@yahoo.com>
2024-08-30 23:04:33 -0700

    added fix for when links = 0, resources should not show up

--- events/ready.js
+++ events/ready.js
@@ -6,42 +6,53 @@ const canvasAPI = require("../core/canvasAPI")
 async function canvaChecker(client) {
     await canvasAPI.fetchAnnouncements().then((announcements) => {
         if (announcements) {
-            const channel = client.channels.cache.get(config.CHANNEL_ID)
-            console.log("Sending Announcements to " + channel.name)
+            const channel = client.channels.cache.get(config.CHANNEL_ID);
+            console.log("Sending Announcements to " + channel.name);
 
             const urlBttn = new ButtonBuilder()
-            .setLabel("View Announcement")
-            .setStyle(ButtonStyle.Link)
-            .setURL(announcements.url)
+                .setLabel("View Announcement")
+                .setStyle(ButtonStyle.Link)
+                .setURL(announcements.url);
 
-            let buttons = []
-            buttons.push(urlBttn)
+            let buttons = [];
+            buttons.push(urlBttn);
 
-            const row = new ActionRowBuilder()
+            const row = new ActionRowBuilder();
+            row.addComponents(buttons);
 
-            const embed = new EmbedBuilder()
-                .setTitle('Resources')
-                .setDescription("Here are some of the links you can find in this announcement!")
-                .setColor(0x0099ff)
-                .setTimestamp();
+            // Initialize an empty embeds array
+            let embeds = [];
 
-            // Add each link to the embed
-            announcements.links.forEach((link, index) => {
-                const url = new URL(link);
-                if (url.pathname == "/")
-                {
-                    embed.addFields({ name: `Link ${index + 1}`, value: `[${url.hostname.replace(/\b\w/g, (c) => c.toUpperCase())}](${link})` });
-                }
-                else {
-                    embed.addFields({ name: `Link ${index + 1}`, value: `[${url.pathname.replaceAll('/', ' ').replaceAll('-', ' ').replace(/\b\w/g, (c) => c.toUpperCase())}](${link})` });
-                }
+            if (announcements.links.length > 0) {
+                const embed = new EmbedBuilder()
+                    .setTitle('Resources')
+                    .setDescription("Here are some of the links you can find in this announcement!")
+                    .setColor(0x0099ff)
+                    .setTimestamp();
+
+                // Add each link to the embed
+                announcements.links.forEach((link, index) => {
+                    const url = new URL(link);
+                    if (url.pathname == "/") {
+                        embed.addFields({ name: `Link ${index + 1}`, value: `[${url.hostname.replace(/\b\w/g, (c) => c.toUpperCase())}](${link})` });
+                    } else {
+                        embed.addFields({ name: `Link ${index + 1}`, value: `[${url.pathname.replaceAll('/', ' ').replaceAll('-', ' ').replace(/\b\w/g, (c) => c.toUpperCase())}](${link})` });
+                    }
+                });
+
+                // Add the embed to the embeds array
+                embeds.push(embed);
+            }
+
+            // Send the message with the embeds array
+            channel.send({
+                content: `@everyone **${announcements.author}** just posted an announcement!`,
+                files: [{ attachment: `./cache/announcement.png`, name: 'announcement.png' }],
+                components: [row],
+                embeds: embeds
             });
-
-            row.addComponents(buttons)
-
-            channel.send({content: `@everyone **${announcements.author}** just posted an announcement!`, files: [{attachment: `./cache/announcement.png`, name: 'announcement.png'}], components: [row], embeds: [embed]})
         }
-    })
+    });
 }
 
 module.exports = {
